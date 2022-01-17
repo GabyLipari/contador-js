c2c24a8d5f6cd702dc7bd31e3c9d7ada36978293
GabyLipari <gabriella.lipari@hotmail.com>
2022-01-17 18:08:39 -0300

    atualização JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,8 @@ var currentNumberWrapper = document.getElementById("currentNumber")
 var currentNumber = 0
 var soma = document.getElementById("increment")
 var subtrai = document.getElementById("decrement")
+var msgPlus = document.querySelector('#messagePlus')
+var msgMinus = document.querySelector('#messageMinus')
  
 function plusOne(){
     currentNumber = currentNumber + 1;
@@ -11,7 +13,8 @@ function plusOne(){
         document.getElementById("currentNumber").style.color = "black";
         }
     if (currentNumber >= 10){
-        soma.removeEventListener('click', plusOne)
+        currentNumber = 9;
+        msgPlus.classList.toggle('revealPlus');
     }
     
 }
@@ -23,9 +26,11 @@ function plusOne(){
         document.getElementById("currentNumber").style.color = "red";
         }
     if (currentNumber <= -10){
-        subtrai.removeEventListener('click', minusOne)
-    }        
-}
+        currentNumber = -9;
+        msgMinus.classList.toggle('revealMinus');
+    }
+}        
+
 
 soma.addEventListener('click', plusOne)
 subtrai.addEventListener('click', minusOne)
